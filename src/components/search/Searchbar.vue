7ad0b2ba0e286c755e54e4703bb297744bee8e6f
Pallavi Debnath <Pallavi.Debnath@sennder.com>
2023-07-11 22:34:56 +0200

    design system

---
+++ src/components/search/Searchbar.vue
@@ -0,0 +1,37 @@
+<template>
+  <header class="toolbar">
+    <nav>
+      <ul>
+        <li><a href="/public">Home</a></li>
+        <li><a href="/search">Search</a></li>
+        <!-- Add more navigation links as needed -->
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+.toolbar {
+  background-color: #f0f0f0;
+  padding: 10px;
+  position: fixed;
+  top: 0;
+  width: 100%;
+}
+
+.toolbar ul {
+  display: flex;
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+
+.toolbar li {
+  margin-right: 10px;
+}
+
+.toolbar a {
+  text-decoration: none;
+  color: #333;
+}
+</style>
